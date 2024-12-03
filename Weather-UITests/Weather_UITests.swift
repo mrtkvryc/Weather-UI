43adea96033d8a152cad6b6082313b19dfb78539
mrtkvryc <dwalin9696@gmail.com>
2024-12-03 20:41:43 +0300

    Initial Commit

---
+++ Weather-UITests/Weather_UITests.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_UITests.swift
+//  Weather-UITests
+//
+//  Created by GTMAC15 on 3.12.2024.
+//
+
+import Testing
+@testable import Weather_UI
+
+struct Weather_UITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
